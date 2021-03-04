26f4373a58b940885395e629e7afb77643eae62a
Minal-Mahor <78798466+Minal-Mahor@users.noreply.github.com>
2021-03-04 16:41:32 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,10 @@
-num1=10
-num2=20
-sum=num1+num2
-print(f'The Sum of two numbers = {sum}')
+from flask import Flask 
+app = Flask(__name__) 
+  
+@app.route('/') 
+def hello(): 
+    return "!!!!!!!!!HELLO WORLD!!!!!!!"
+  
+  
+if __name__ == "__main__": 
+    app.run(host ='0.0.0.0', port = 5001, debug = True)
